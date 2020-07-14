729309d5e3bc787a2d033c2232d304b8f275e04f
amitmse <amitmse@gmail.com>
2020-07-14 16:29:24 +0530

    Update Plot.py

--- plot/Plot.py
+++ plot/Plot.py
@@ -32,47 +32,25 @@ df = input_data[:]
 df.head()
 
 #####################################################################
-###### Bar Graph using matplotlib ####################
-#plt.bar(X Axis, Y Axis)
-plt.bar(df.reservation_c,df['NOP_before_purchase'])
+###### matplotlib ####################################
+X_axis='home_ownership'
+Y_axis='recoveries'
+
+plt.bar(df[X_axis],df[Y_axis])
+
 plt.xticks(rotation=70) 
-plt.xlabel('Booking')
-plt.ylabel('# Pages Visit in last time')
-plt.title('Booking by #Page visits')
+plt.xlabel(X_axis)
+plt.ylabel(Y_axis)
+plt.title(X_axis + ' by ' + Y_axis)
 #plt.savefig(r"C:\Users\AMIT\Google Drive\Study\ML\07.Boosting\matplotlib_plotting_6.png", dpi=300,bbox_inches='tight') 
 #plot plt.show();
 plt.show();
+######################
+plt.bar(df[X_axis],df[Y_axis])
+plt.plot(df[X_axis],df[Y_axis])
+plt.scatter(df[X_axis],df[Y_axis])
+plt.hist(df[Y_axis])
+plt.pie(df[Y_axis])
+########################################################
 
-########################################################################
-###### Bar Graph using matplotlib ####################
-#plt.bar(X Axis, Y Axis)
-#plt.plot(df['NOP_before_purchase'])
-#plt.plot(df['NOP_before_purchase'], 'ro')
-#plt.plot(df['NOP_before_purchase'], 'bs')
-#plt.scatter(df.reservation_c,df['NOP_before_purchase'])
-plt.xticks(rotation=70) 
-plt.xlabel('Booking')
-plt.ylabel('# Pages Visit in last time')
-plt.title('Booking by #Page visits')
-#plt.savefig(r"C:\Users\AMIT\Google Drive\Study\ML\07.Boosting\matplotlib_plotting_6.png", dpi=300,bbox_inches='tight') 
-#plot plt.show();
-plt.show();
-########################################################################
-plt.hist(df['nop_last_visit'],rwidth=0.9,alpha=0.3,color='blue',bins=15,edgecolor='red')
 
-#x and y-axis labels 
-plt.xlabel('# Pages Visit in last time') 
-plt.ylabel('# Visitors') 
-#plot title 
-plt.title('Booking by #Page visits') 
-#save and display the plot 
-#plt.savefig(r"C:\Users\AMIT\Google Drive\Study\ML\07.Boosting\matplotlib_plotting_10.png",dpi=300,bbox_inches='tight') 
-plt.show();
-########################################################################
-fig,ax=plt.subplots(nrows=1,ncols=1,figsize=(20,5)) 
-
-#manipulating the first Axes 
-ax[0].plot(week,df['nop_last_visit']) 
-ax[0].set_xlabel('Week') 
-ax[0].set_ylabel('Revenue') 
-ax[0].set_title('Weekly income') 
