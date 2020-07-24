1ce62198efb3156510b79b4128d59df83220c0ed
amitmse <amitmse@gmail.com>
2020-07-24 15:57:01 +0530

    Update Plot.py

--- plot/Plot.py
+++ plot/Plot.py
@@ -19,7 +19,17 @@ import seaborn as sns
 ##########################################
 from plotnine import *
 ##########################################
-
+import pandas_bokeh                               # This is Pandas-bokeh
+pd.set_option('plotting.backend', 'pandas_bokeh') # This is Pandas-bokeh
+#########################################
+from bokeh.plotting import figure, output_file, show
+from bokeh.io import output_notebook
+from bokeh.models import HoverTool, BoxSelectTool, ColumnDataSource #For enabling tools
+from bokeh.sampledata.autompg import autompg_clean as df
+from bokeh.transform import factor_cmap
+from bokeh.layouts import column
+from bokeh.models import CustomJS, ColumnDataSource, Slider
+# Go to => https://docs.bokeh.org/en/latest/docs/user_guide.html#userguide => click on "Tutorial"
 ##########################################
 plt.style.use('seaborn')
 ##########################################
@@ -92,9 +102,100 @@ X_axis_cat_2='income_category'
 #ggplot(df, aes(x=X_axis , y=Y_axis )) + geom_line()                                # Line
 #ggplot(df, aes(x=X_axis )) + geom_histogram()                                      # Histogram
 ############################################################################################################
+######### Pandas bokeh ###########################################
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas_bokeh                               # This is Pandas-bokeh
+pd.set_option('plotting.backend', 'pandas_bokeh') # This is Pandas-bokeh
 
+###################################
+# Line Plots #
+df = pd.DataFrame(np.random.randn(1000, 4), index=df.index, columns=list('ABCD'))
+df = df.cumsum()
+plt.figure()
+df.plot()
+###################################
+# Scatter Plots with Pandas-bokeh #
+df = pd.DataFrame(np.random.rand(50, 4), columns=['a', 'b', 'c', 'd'])
+df.plot.scatter(x='a', y='b')
+###################################
+# Histogram #
+df = pd.DataFrame({'a': np.random.randn(1000) + 1, 'b': np.random.randn(1000),'c': np.random.randn(1000) - 1}, columns=['a', 'b', 'c'])
+plt.figure()
+df.plot.hist(stacked=True, bins=20)
+###################################
+# Histogram with Pandas-bokeh #
+df = pd.DataFrame({'a': np.random.randn(1000) + 1, 'b': np.random.randn(1000),'c': np.random.randn(1000) - 1}, columns=['a', 'b', 'c'])
+plt.figure();
+df.plot.hist(alpha=0.5)
 ############################################################################################################
+######### bokeh ###########################################
+X_axis='annual_inc'
+Y_axis='recoveries'
+##################
+X_axis_cat_1='home_ownership'
+X_axis_cat_2='income_category'
+##################
+# prepare some data
+x= df[X_axis].values.tolist()
+y= df[Y_axis].values.tolist()
+#######################################
+# output to static HTML file (with CDN resources)
+output_notebook()
+output_file("dot.html")
 
+TOOLS = [BoxSelectTool(), HoverTool()]
+
+# create a new plot with the tools above, and explicit ranges
+#p = figure()
+p = figure(tools=TOOLS)
+# add a circle renderer with vectorized colors and sizes
+####################################################
+#p.line(x,y)                    #Line
+p.circle(x,y,width=8)           #dot
+#p.triangle(x,y)                #triangle
+#p.vbar(x,top=y, width=2)       #bar
+#p.hbar(x,right=y, height =2)   #bar
+#p.quad(x,y)                    #triangle
+#p.square(x, y, size=10)
+
+# show the results
+show(p)
+####################################################
+#### Slider
+#https://hub.gke.mybinder.org/user/bokeh-bokeh-notebooks-zzw15vsm/notebooks/tutorial/06%20-%20Linking%20and%20Interactions.ipynb
+#https://docs.bokeh.org/en/latest/docs/user_guide.html#userguide
+#Linking and Interactions
+from bokeh.layouts import column
+from bokeh.models import CustomJS, ColumnDataSource, Slider
+
+x = [x*0.005 for x in range(0, 201)]
+
+source = ColumnDataSource(data=dict(x=x, y=x))
+
+plot = figure(plot_width=400, plot_height=400)
+plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
+
+slider = Slider(start=0.1, end=6, value=4, step=.1, title="power")
+
+update_curve = CustomJS(args=dict(source=source, slider=slider), code="""
+    var data = source.data;
+    var f = slider.value;
+    var x = data['x']
+    var y = data['y']
+    for (var i = 0; i < x.length; i++) {
+        y[i] = Math.pow(x[i], f)
+    }
+    
+    // necessary becasue we mutated source.data in-place
+    source.change.emit();
+""")
+slider.js_on_change('value', update_curve)
+
+show(column(slider, plot))
+
+#https://s3.amazonaws.com/assets.datacamp.com/production/course_2244/slides/ch4_slides.pdf
 ############################################################################################################
 
 ############################################################################################################
