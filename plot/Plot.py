f567152239c7b6c9f381fbeb01168c98c6cf38a5
amitmse <amitmse@gmail.com>
2025-03-16 13:09:18 +0530

    Update Plot.py

--- plot/Plot.py
+++ plot/Plot.py
@@ -19,89 +19,116 @@ import seaborn as sns
 ##########################################
 from plotnine import *
 ##########################################
-import pandas_bokeh                               # This is Pandas-bokeh
-pd.set_option('plotting.backend', 'pandas_bokeh') # This is Pandas-bokeh
+#import pandas_bokeh                               # This is Pandas-bokeh
+#pd.set_option('plotting.backend', 'pandas_bokeh') # This is Pandas-bokeh
 #########################################
 from bokeh.plotting import figure, output_file, show
 from bokeh.io import output_notebook
 from bokeh.models import HoverTool, BoxSelectTool, ColumnDataSource #For enabling tools
-from bokeh.sampledata.autompg import autompg_clean as df
+#from bokeh.sampledata.autompg import autompg_clean as df
 from bokeh.transform import factor_cmap
 from bokeh.layouts import column
 from bokeh.models import CustomJS, ColumnDataSource, Slider
+from bokeh.io import export_png
 # Go to => https://docs.bokeh.org/en/latest/docs/user_guide.html#userguide => click on "Tutorial"
 ##########################################
-plt.style.use('seaborn')
+#plt.style.use('seaborn')
 ##########################################
 
-#os.chdir(r"C:\Users\AMIT\Google Drive\Study\ML\07.Boosting")
-#input_data = pd.read_csv('Dev1_Hilton_Model_Data.csv')
-#df= input_data[:]
-#df['target'] = np.where(df['reservation']>0, 'Yes', 'No')
-#df = df.set_index('target')
-#df.head()
 ##########################################
 
-os.chdir(r"C:\Users\AMIT\Google Drive\Study\ML\06.Random_Forest")
+os.chdir(r"G:\My Drive\Study\ML\06.Random_Forest")
 input_data = pd.read_csv('IV_Data.csv')
 df = input_data[:]
 df.head()
 
-############################################################################################################
+##########################################
+
 ###### matplotlib ####################################
-X_axis='home_ownership'
-Y_axis='recoveries'
+
+X_axis='browser_cat'
+Y_axis='NOP_before_purchase'
+
+#############################################
 
 plt.bar(df[X_axis],df[Y_axis])
+#plt.plot(df[X_axis],df[Y_axis])
+#plt.scatter(df[X_axis],df[Y_axis])
+#plt.hist(df[Y_axis])
+#plt.pie(df[Y_axis])
+
+#############################################
 
 plt.xticks(rotation=70) 
 plt.xlabel(X_axis)
 plt.ylabel(Y_axis)
 plt.title(X_axis + ' by ' + Y_axis)
-#plt.savefig(r"C:\Users\AMIT\Google Drive\Study\ML\07.Boosting\matplotlib_plotting_6.png", dpi=300,bbox_inches='tight') 
-#plot plt.show();
+plt.savefig(r"G:\My Drive\Study\ML\06.Random_Forest\matplotlib_bar.png", dpi=300,bbox_inches='tight') 
 plt.show();
-######################
-plt.bar(df[X_axis],df[Y_axis])
-plt.plot(df[X_axis],df[Y_axis])
-plt.scatter(df[X_axis],df[Y_axis])
-plt.hist(df[Y_axis])
-plt.pie(df[Y_axis])
-###############################################################################################################
+
+#######################################################
+
+
 ######### Seaborn #####################################
-#X_axis='home_ownership'
-X_axis='annual_inc'
-Y_axis='recoveries'
+X_axis='browser_cat'
+Y_axis='NOP_before_purchase'
 
 sns.barplot(x=X_axis, y=Y_axis, data = df)                        # Bar plot
-sns.pointplot(x=X_axis, y=Y_axis, data = df)                      # Line with dot
-sns.relplot(x=X_axis, y=Y_axis, data = df)                        # scatter plot
-sns.distplot(df[Y_axis], kde=False, rug = True)                   # Histograms 
-sns.countplot(x=X_axis, data = df)                                # Bar chart. Show the counts of observations in each categorical bin.
-sns.jointplot(x=X_axis, y=Y_axis, data = df)                      # bivariate and univariate graphs
-sns.catplot(x=X_axis, y=Y_axis, data = df)                        # scatterplots (Categorical)
-sns.regplot(x=X_axis, y=Y_axis, data = df)                        # linear regression model fit
-sns.lmplot(x=X_axis, y=Y_axis, data = df)                         # Combination of regplot & FacetGrid. 
-sns.residplot(x=X_axis, y=Y_axis, data=df);                       # Residual values
+#sns.pointplot(x=X_axis, y=Y_axis, data = df)                      # Line with dot
+#sns.relplot(x=X_axis, y=Y_axis, data = df)                        # scatter plot
+#sns.distplot(df[Y_axis], kde=False, rug = True)                   # Histograms 
+#sns.countplot(x=X_axis, data = df)                                # Bar chart. Show the counts of observations in each categorical bin.
+#sns.jointplot(x=X_axis, y=Y_axis, data = df)                      # bivariate and univariate graphs
+#sns.catplot(x=X_axis, y=Y_axis, data = df)                        # scatterplots (Categorical)
+
 ############################################################################################################
+
+#sns.regplot(x='reservation_n', y=Y_axis, data = df)               # linear regression model fit
+#sns.lmplot(x='reservation_n', y=Y_axis, data = df)                # Combination of regplot & FacetGrid.
+#sns.residplot(x='reservation_n', y=Y_axis, data=df);              # Residual values
+
+############################################################################################################
+
+plt.savefig(r"G:\My Drive\Study\ML\06.Random_Forest\Seaborn_bar.png", dpi=300,bbox_inches='tight') 
+
+plt.show()
+
 ######### ggplot ###########################################
-X_axis='annual_inc'
-Y_axis='recoveries'
-##################
-X_axis_cat_1='home_ownership'
-X_axis_cat_2='income_category'
-##################
-#https://medium.com/@gscheithauer/data-visualization-in-python-like-in-rs-ggplot2-bc62f8debbf5
+X_axis='reservation_n'
+Y_axis='NOP_before_purchase'
+X_axis_cat_1 = 'browser_cat'
+X_axis_cat_2 = 'dd'
 ##################
 
 #ggplot(df, aes(x=X_axis , y=Y_axis )) + geom_point()                               # Scatter
-#ggplot(df, aes(x=X_axis_cat_1)) + geom_bar()                                       # Bar
+a=ggplot(df, aes(x=X_axis_cat_1)) + geom_bar()                                      # Bar
 #ggplot(df, aes(x=X_axis_cat_1)) + geom_bar() + coord_flip()                        # Bar: Flip
 #ggplot(df, aes(x=X_axis_cat_1)) + geom_bar(stat = 'count')                         # Bar: Count
 #ggplot(df, aes(x=X_axis_cat_1, fill = X_axis_cat_2)) + geom_bar(stat = 'count')    # Bar: Stack with count
 #ggplot(df, aes(x=X_axis , y=Y_axis )) + geom_line()                                # Line
 #ggplot(df, aes(x=X_axis )) + geom_histogram()                                      # Histogram
 ############################################################################################################
+
+a.save(r"G:\My Drive\Study\ML\06.Random_Forest\ggplot_bar.png")
+
+#plt.show()
+
+
+######### Bokeh ###########################################
+X_axis='reservation_n'
+Y_axis='NOP_before_purchase'
+X_axis_cat_1 = 'browser_cat'
+X_axis_cat_2 = 'dd'
+##################
+# instantiating the figure object  
+#graph = figure(title = "Bokeh Bar Graph")  
+# plotting the bar graph  
+graph.hbar(df[X_axis], right =df[Y_axis], height = 0.5)  
+
+#export_png(a, filename = r"G:\My Drive\Study\ML\06.Random_Forest\bokeh_bar.png") 
+# displaying the model  
+show(graph)
+
 ######### Pandas bokeh ###########################################
 import pandas as pd
 import numpy as np
@@ -197,11 +224,3 @@ show(column(slider, plot))
 
 # Slider : https://s3.amazonaws.com/assets.datacamp.com/production/course_2244/slides/ch4_slides.pdf
 # Real Time: https://stackoverflow.com/questions/37724660/streaming-two-line-graphs-using-bokeh
-
-############################################################################################################
-
-############################################################################################################
-
-############################################################################################################
-
-############################################################################################################
